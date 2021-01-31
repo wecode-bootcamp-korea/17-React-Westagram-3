9527243b29933844688f0919f8d497c3436a8ff7
JS KWAK <75110649+Kwakjinseok@users.noreply.github.com>
2021-01-31 20:40:30 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/Pages/kwakjinseok/Login/Login.js
+++ src/Pages/kwakjinseok/Login/Login.js
@@ -3,12 +3,33 @@ import { withRouter } from "react-router-dom";
 import "./Login.scss";
 
 class LoginKwak extends Component {
+	constructor() {
+		super();
+		this.state = {
+			idInputValue: "",
+			pwInputValue: "",
+		};
+	}
+
+	handleIdInput = (event) => {
+		this.setState({
+			idInputValue: `${event.target.value}`,
+		});
+	};
+
+	handlePwInput = (event) => {
+		this.setState({
+			pwInputValue: `${event.target.value}`,
+		});
+	};
+
 	goToMain = () => {
 		if (this.goToMain) {
 			alert("안녕하세요");
 		}
 		this.props.history.push("/mainKwak");
 	};
+
 	render() {
 		return (
 			<div>
@@ -21,12 +42,14 @@ class LoginKwak extends Component {
 							id="login"
 							type="text"
 							placeholder="전화번호,사용자 이름 또는 이메일"
+							onChange={this.handleIdInput}
 						/>
 						<input
 							className="weAreInput"
 							id="password"
 							type="password"
 							placeholder="비밀번호"
+							onChange={this.handlePwInput}
 						/>
 
 						<button onClick={this.goToMain}>로그인</button>
