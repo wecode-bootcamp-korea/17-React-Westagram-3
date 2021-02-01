f5574232f32f51979067f0266345074cb5f58a0d
PJH <carpediem.mementomori.1984@gmail.com>
2021-02-01 21:22:22 +0900

    Modify: mock data 활용 여러개 피드 구현 완료(Mission 2)

--- src/Pages/parkjunghyun/Main/components/Feeds.js
+++ src/Pages/parkjunghyun/Main/components/Feeds.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Comments from '../Comments';
+import Feed from '../Feed';
 import './MainLeft.scss';
 import './Feeds.scss';
 
@@ -9,6 +10,7 @@ export default class Feeds extends Component {
     this.state = {
       comments: [],
       commentValue: '',
+      feeds: [],
     };
 
     this.addPost = this.addPost.bind(this);
@@ -25,6 +27,16 @@ export default class Feeds extends Component {
           comments: data,
         });
       });
+
+    fetch('http://localhost:3000/data/feedsData.json', {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          feeds: data,
+        });
+      });
   }
 
   handleCommentValue(e) {
@@ -35,6 +47,7 @@ export default class Feeds extends Component {
 
   addPost = (e) => {
     e.preventDefault();
+    console.log(this.state.commentValue);
 
     const { comments, commentValue } = this.state;
 
@@ -55,47 +68,31 @@ export default class Feeds extends Component {
   render() {
     return (
       <article className="feeds">
-        <div className="author">
-          <img alt="profile" src="images/parkjunghyun/profile.jpeg" />
-          <div className="authorText">
-            <div id="authorName">
-              <b>gongganlog</b>
-            </div>
-            <div id="location">경주</div>
-          </div>
-          <img alt="more" id="more" src="images/parkjunghyun/more.png" />
-        </div>
-        <img alt="instaImg" src="images/parkjunghyun/instaImg.png" />
-        <div className="feedIcons">
-          <img alt="like" src="images/parkjunghyun/heart.png" />
-          <img alt="msgbubble" src="images/parkjunghyun/msgbubble.png" />
-          <img alt="sendicon" src="images/parkjunghyun/send.png" />
-          <img alt="bookmark" src="images/parkjunghyun/bookmark.png" />
-        </div>
-        <div id="likes">
-          <b>좋아요 4개</b>
-        </div>
-        <div id="comment">
-          <b>gongganlog |</b>
-          <a href="www.google.com">#경주전원주택..</a>
-        </div>
-        <div id="writeTime">2분전</div>
-        <form className="writeCmt" onSubmit={this.addPost}>
-          <input
-            type="text"
-            className="postingCmt"
-            placeholder="댓글 달기..."
-            onChange={this.handleCommentValue}
-          />
-          <button id="post">게시</button>
-        </form>
-        {this.state.comments.map((comment) => {
+        {this.state.feeds.map((feed) => {
           return (
-            <Comments
-              key={comment.id}
-              text={comment.content}
-              like={comment.isLiked}
-            />
+            <div key={feed.id}>
+              <Feed
+                key={feed.id}
+                authorImgSrc={feed.authorImgSrc}
+                authorName={feed.authorName}
+                authorLocation={feed.authorLocation}
+                mainImgSrc={feed.mainImgSrc}
+                commentAuthor={feed.commentAuthor}
+                commentHash={feed.commentHash}
+                commentTime={feed.commentTime}
+                handleCommentValue={this.handleCommentValue}
+                addPost={this.addPost}
+              />
+              {this.state.comments.map((comment) => {
+                return (
+                  <Comments
+                    key={comment.id}
+                    text={comment.content}
+                    like={comment.isLiked}
+                  />
+                );
+              })}
+            </div>
           );
         })}
       </article>
